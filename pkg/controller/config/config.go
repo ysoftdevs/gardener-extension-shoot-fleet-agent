40d7b9b8725124ba134e290db77a732f4fae8d59
Jakub Vavřík <jakub.vavrik@ysoft.com>
2021-01-28 18:57:14 +0100

    minor changes to make linter happy#3

--- pkg/controller/config/config.go
+++ pkg/controller/config/config.go
@@ -2,7 +2,7 @@ package config
 
 import "github.com/javamachr/gardener-extension-shoot-fleet-agent/pkg/apis/config"
 
-//Holds controller config
+// Config holds controller config
 type Config struct {
 	config.FleetAgentConfig
 }
