9495e8e20089800dfc1b97c8cb08a99292e65791
Jakub Vavřík <jakub.vavrik@ysoft.com>
2021-01-28 19:01:44 +0100

    minor changes to make linter happy#4

--- pkg/apis/config/doc.go
+++ pkg/apis/config/doc.go
@@ -1,6 +1,6 @@
+// Package config that holds config api
 // +k8s:deepcopy-gen=package
 // +groupName="shoot-fleet-agent-service.extensions.config.gardener.cloud"
 
 //go:generate ../../../hack/update-codegen.sh
-// Package config that holds config api
 package config
